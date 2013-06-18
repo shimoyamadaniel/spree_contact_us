13b6e2a3e6ecffebc1ece3224d0abb37af21a1cc
Daniel Shimoyama <shimoyama.daniel@gmail.com>
2013-06-18 11:15:45 -0300

    merge

--- app/controllers/spree/inquiries_controller.rb
+++ app/controllers/spree/inquiries_controller.rb
@@ -1,16 +1,20 @@
-class Spree::InquiriesController < Spree::StoreController
-  
-	def new
-		@inquiry = Spree::Inquiry.new
-  end
+module Spree
 
-  def create
-		@inquiry = Spree::Inquiry.new(params[:inquiry])
-		if @inquiry.save
-			redirect_to(contact_path, notice: Spree.t(:on_contact_us))
-		else
-			render :new
+	class InquiriesController < Spree::StoreController
+
+		def new
+			@inquiry = Spree::Inquiry.new
 		end
-  end
+
+		def create
+			@inquiry = Spree::Inquiry.new(params[:inquiry])
+			if @inquiry.save
+				redirect_to(success_contact_path, notice: Spree.t(:on_contact_us))
+			else
+				render :new
+			end
+		end
+
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Spree::Core::Engine.routes.draw do
 
 	resources :inquiries, only: [:new, :create]
 	
-	match '/contact' => 'inquiries#new', :as => :contact
+	match '/contact' => 'inquiries#new', :as => :new_contact, :via => :get
+	match '/contact' => 'inquiries#create', :as => :create_contact, :via => :post
+	match '/contact/success' => 'inquiries#success', :as => :success_contact, :via => :get
 
 	namespace :admin do
 		resources :inquiries
