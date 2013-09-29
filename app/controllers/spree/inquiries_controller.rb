beb6aeeb5fafc65878babea0fa09f59d3c00a9c8
Daniel Shimoyama <shimoyama.daniel@gmail.com>
2013-09-29 11:30:34 -0300

    updated to use spree 2.1.*

--- app/controllers/spree/inquiries_controller.rb
+++ app/controllers/spree/inquiries_controller.rb
@@ -2,18 +2,21 @@ module Spree
 
 	class InquiriesController < Spree::StoreController
 
+    respond_to :html
+
 		def new
 			@inquiry = Spree::Inquiry.new
 		end
 
 		def create
-			@inquiry = Spree::Inquiry.new(params[:inquiry])
-			if @inquiry.save
-				redirect_to(success_contact_path, notice: Spree.t(:on_contact_us))
-			else
-				render :new
-			end
+			@inquiry = Spree::Inquiry.new(inquiry_params)
+			flash[:notice] = Spree.t(:on_contact_us) if @inquiry.save
+			respond_with(@inquiry, location: success_contact_path)
 		end
+
+    def inquiry_params
+      params.require(:inquiry).permit(:full_name, :email, :phone_number, :mobilephone_number, :message, :inquiry_type)
+    end
 
 	end
 
