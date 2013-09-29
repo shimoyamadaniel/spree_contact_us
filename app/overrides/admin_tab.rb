6f981c6083aca3dd360a388263914484fe7844ec
Daniel Shimoyama <shimoyama.daniel@gmail.com>
2013-09-29 11:44:05 -0300

    Fixed Deface for admin tab

--- app/overrides/admin_tab.rb
+++ app/overrides/admin_tab.rb
@@ -1,7 +1,8 @@
 Deface::Override.new(
-    :virtual_path => "spree/layouts/admin",
+    :virtual_path => "spree/admin/shared/_menu",
     :name => "spree_inquiries_admin_tag",
-    :insert_bottom => "[data-hook='admin_tabs']",
+    :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
     :text => "<%= tab(:contacts, :url => spree.admin_inquiries_path, :icon => 'icon-email') %>",
-    :disabled => false
+    :disabled => false,
+    :original => '9a0d7928b4973c2395de948e9df33a748ea0aa8a'
 )
